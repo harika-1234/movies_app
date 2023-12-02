120f3d27a603267932c9921975cf783bd87564b7
root <narasimha@gmail.com>
2023-12-02 05:37:47 +0000

    movieapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import './App.css'
-
-const App = () => <div>Hello World</div>
-
-export default App
 import {Switch, Route, Redirect} from 'react-router-dom'
 import LoginRoute from './components/LoginRoute/index'
-import Home from './components/Home/index'
+import HomeSection from './components/HomeSection/index'
 import PopularSection from './components/PopularSection/index'
 import AccountSection from './components/AccountSection/index'
 import SearchRoute from './components/SearchRoute/index'
@@ -17,7 +12,7 @@ import './App.css'
 const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginRoute} />
-    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/" component={HomeSection} />
     <ProtectedRoute exact path="/popular" component={PopularSection} />
     <ProtectedRoute exact path="/account" component={AccountSection} />
     <ProtectedRoute exact path="/search" component={SearchRoute} />
